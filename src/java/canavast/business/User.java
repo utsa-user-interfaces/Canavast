1aa58afcbfad2741958705aecdb24f63490d5bc5
gsessums <geoffreysessums@gmail.com>
2018-11-10 17:07:05 -0600

    Updated User.java

--- src/java/canavast/business/User.java
+++ src/java/canavast/business/User.java
@@ -8,36 +8,16 @@ import java.io.Serializable;
  */
 public class User implements Serializable {
 
-    private String firstName;
-    private String lastName;
     private String email;
 
     public User() {
-        firstName = "";
-        lastName = "";
+
         email = "";
     }
 
-    public User(String firstName, String lastName, String email) {
-        this.firstName = firstName;
-        this.lastName = lastName;
+    public User(String email) {
+
         this.email = email;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
     }
 
     public String getEmail() {
