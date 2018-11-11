65827e41d2140f3d9217533be796b1172d3b69dc
gsessums <geoffreysessums@gmail.com>
2018-11-10 18:41:17 -0600

    Added the package canavast.data

---
+++ src/java/canavast/data/UserDB.java
@@ -0,0 +1,16 @@
+package canavast.data;
+
+import canavast.business.User;
+
+/**
+ *
+ * @author Geoffrey Sessums
+ */
+public class UserDB {
+
+    public static long insert(User user) {
+        // TODO: Add code that adds the user to the database
+        // NOTE: This is shown in chapters 11-13
+        return 0;
+    }
+}
